9c0961020e9f43131079d3d1c8858bec545dd0eb
DataArtistMan <mark051405140514@gmail.com>
2023-06-25 16:06:40 +0900

    3

--- diary/auth.py
+++ diary/auth.py
@@ -1,14 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 auth = Blueprint('auth', __name__)
 
-@auth.route('/sign-in')
+@auth.route('/sign_in', methods = ['GET', 'POST'])
 def sign_in():
-    return "<p>Sign-In</p>"
+    return render_template('sign_in.html', user = 'kim')
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign_up', methods = ['GET','POST'])
 def sign_up():
-    return "<p>Sign-Up</p>"
+    # 데이터 확인
+    if request.method == 'POST':
+        nickname = request.form.get('nickname')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 5:
+            pass
+        elif len(nickname) < 2:
+            pass
+        elif password1 != password2 :
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            pass  # Create User -> DB
+    data = request.form
+    print(data) # POST로 요청할때만 값나옴
+    return render_template('sign_up.html')
